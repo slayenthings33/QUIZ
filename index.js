d07aec0d0d7eda6c1f983c2e26596ac1e8d6c6a5
Ryan Slayen <ryanslayen@gmail.com>
2020-08-06 12:10:27 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let answer1 = document.getElementById("answer1");
+let answer2 = document.getElementById("answer2");
+let answer3 = document.getElementById("answer3");
+let answer4 = document.getElementById("answer4");
+let question = document.getElementById("question");
+let quizPic = document.getElementById("quizPic");
+let nextQuestion = document.getElementById("nextQuestion");
+
+let submittedAnswers = [];
+let arrQuestions = ["What is the spiciest pepper in the world?", "WHat animal kills the most humans each year?", "When was the European Union formed?", "Which music group sold the most records?", "What is most popular food globally?"];
+let correctAnswers = ["The Carolina Reaper", "The Mosquito", "November 1993", "The Beatles", "Pizza"];
+
+
+// QUESTIONS: What is the spiciest pepper in the world? (carolina reaper) ; What animal kills the most people each year? (mostquito) ; When was the European Union formed? (November 1993) ; Which music group sold the most records? (Beatles) ; What is the best food in the world? (pizza) 
+
