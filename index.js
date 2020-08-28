a9d27da41ef45a39f9e1ab3452162f8b4afc09cf
Ryan Slayen <ryanslayen@gmail.com>
2020-08-28 10:18:35 +0200

    not checking answers

--- index.js
+++ index.js
@@ -18,7 +18,18 @@ let submittedAnswers = [];
 /**********IMAGES*********/
 /*************************/
 
-let images = ["./img/peppers.jpeg", "./img/graveyard.jpeg", "./img/euUnion.jpg", "./img/concert.jpg", "./img/longRiver.jpeg", "./img/oldTree.jpg", "./img/microwave.jpeg", "./img/virus.png", "./img/cow.jpg", "./img/html.jpg"];
+let images = [
+    "./img/peppers.jpeg",
+    "./img/graveyard.jpeg",
+    "./img/euUnion.jpg",
+    "./img/concert.jpg",
+    "./img/longRiver.jpeg",
+    "./img/oldTree.jpg",
+    "./img/microwave.jpeg",
+    "./img/virus.png",
+    "./img/cow.jpg",
+    "./img/html.jpg"
+];
 
 
 /*************************/
@@ -32,60 +43,72 @@ let questions = [
         answers: [
             "Jalapeño", "Ghost Pepper", "Carolina Reaper", "Habanero"
         ],
-        rightAnswer: 2 
+        rightAnswer: 2
     },
     {
         question: "What animal kills the most humans each year?",
         image: "./img/graveyard.jpeg",
-        answers: ["Hippopotamus", "Sharks", "Snakes","Mosquitos"],
+        answers: [
+            "Hippopotamus", "Sharks", "Snakes", "Mosquitos"
+        ],
         rightAnswer: 3
     },
     {
         question: "When was the European Union formed?",
         image: "./img/euUnion.jpg",
-        answers: ["January 1990", "September 1992", "November 1993", "July 1991"],
+        answers: [
+            "January 1990", "September 1992", "November 1993", "July 1991"
+        ],
         rightAnswer: 2
     },
     {
         question: "What album has the highest album sales?",
         image: "./img/concert.jpg",
-        answers: ["Thriller- Michael Jackson", "The White Album- The Beatles", "The Eagles- Greatest Hits", "Led Zeppelin IV- Led Zeppelin"],
+        answers: [
+            "Thriller- Michael Jackson", "The White Album- The Beatles", "The Eagles- Greatest Hits", "Led Zeppelin IV- Led Zeppelin"
+        ],
         rightAnswer: 2
-    },
-    {
+    }, {
         question: "What is the longest river in the world?",
         image: "./img/longRiver.jpeg",
-        answers: ["The Nile", "The Amazon", "The Mississippi", "The Yangtze"],
+        answers: [
+            "The Nile", "The Amazon", "The Mississippi", "The Yangtze"
+        ],
         rightAnswer: 0
-    },
-    {
+    }, {
         question: "How old is the oldest living tree?",
         image: "./img/oldTree.jpg",
-        answers: ["4,851 year old Methusela tree", "4,500 year old Mountain Ash", "8,359 year old Yellow Meranti", "5,950 year old Redwood"],
+        answers: [
+            "4,851 year old Methusela tree", "4,500 year old Mountain Ash", "8,359 year old Yellow Meranti", "5,950 year old Redwood"
+        ],
         rightAnswer: 0
-    },
-    {
+    }, {
         question: "What year was the first microwave sold?",
         image: "./img/microwave.jpeg",
-        answers: ["December 1996", "July, 1992", "January, 1994", "October, 1995"],
+        answers: [
+            "December 1996", "July, 1992", "January, 1994", "October, 1995"
+        ],
         rightAnswer: 3
-    },
-    {
+    }, {
         question: "What is Covid-19?",
         image: "./img/virus.png",
-        answers: ["An endangered species of beetle", "Lamborghini's 2020 model", "The fiber optic cable connecting Europe to the US", "A Virus"],
+        answers: [
+            "An endangered species of beetle", "Lamborghini's 2020 model", "The fiber optic cable connecting Europe to the US", "A Virus"
+        ],
         rightAnswer: 3
-    },
-    {
+    }, {
         question: "How many toes does a cow have?",
         image: "./img/cow.jpg",
-        answers: ["One", "Two", "Three", "Four"],
+        answers: [
+            "One", "Two", "Three", "Four"
+        ],
         "rightAnswer": 1
-    },
-    {
+    }, {
         question: "Is HTML considered a programming language?",
         image: "./img/html.jpg",
-        answers: ["Yes", "No", "Maybe", "I don't know..."],
+        answers: [
+            "Yes", "No", "Maybe", "I don't know..."
+        ],
         rightAnswer: 0
     }
 ];
@@ -95,20 +118,32 @@ let questionCounter = 0;
 
 let questionPage = `
 <main id="flexContainer">
-<p id="question">${questions[questionCounter].question}</p>
-<img id="quizPic" src="${questions[questionCounter].image}" alt="Question Image">
+<p id="question">${
+    questions[questionCounter].question
+}</p>
+<img id="quizPic" src="${
+    questions[questionCounter].image
+}" alt="Question Image">
 <div id="flexAnswers">
 <div>
-<p class="answer" id="answer1">${questions[questionCounter].answers[0]}</p>
+<p class="answer" id="answer1">${
+    questions[questionCounter].answers[0]
+}</p>
 </div>
 <div>
-<p class="answer" id="answer2">${questions[questionCounter].answers[1]}</p>
+<p class="answer" id="answer2">${
+    questions[questionCounter].answers[1]
+}</p>
 </div>
 <div>
-<p class="answer" id="answer3">${questions[questionCounter].answers[2]}</p>
+<p class="answer" id="answer3">${
+    questions[questionCounter].answers[2]
+}</p>
 </div>
 <div>
-<p class="answer"id="answer4">${questions[questionCounter].answers[3]}</p>
+<p class="answer"id="answer4">${
+    questions[questionCounter].answers[3]
+}</p>
 </div>
 </div>    
 <button id="nextQuestion" onclick="change()">Next Question</button>
@@ -121,17 +156,19 @@ let questionPage = `
 /*******************************/
 
 function startQuiz() {
-    startButton.innerHTML = "Next Question"; 
-    flexContainer.innerHTML = questionPage; 
-    
-    
+    startButton.innerHTML = "Next Question";
+    flexContainer.innerHTML = questionPage;
+
+
     let nextQuestion = document.getElementById("nextQuestion");
     nextQuestion.onclick = () => change();
     let answer1 = document.getElementById("answer1");
     let answer2 = document.getElementById("answer2");
     let answer3 = document.getElementById("answer3");
     let answer4 = document.getElementById("answer4");
-    
+
+    let userChoice = [];
+
     answer1.onclick = () => {
         answer1.style.border = "1px black solid";
         answer1.style.fontWeight = "normal";
@@ -149,7 +186,9 @@ function startQuiz() {
         answer4.style.fontWeight = "normal";
         answer4.style.backgroundColor = "#303C6C"
         answer4.style.color = "#B4DFE5"
-    }
+        userChoice.push(0);
+        console.log(userChoice);
+    };
     answer2.onclick = () => {
         answer1.style.border = "1px black solid";
         answer1.style.fontWeight = "normal";
@@ -167,7 +206,9 @@ function startQuiz() {
         answer4.style.fontWeight = "normal";
         answer4.style.backgroundColor = "#303C6C";
         answer4.style.color = "#B4DFE5";
-    }
+        userChoice.push(1);
+        console.log(userChoice);
+    };
     answer3.onclick = () => {
         answer1.style.border = "1px black solid";
         answer1.style.fontWeight = "normal";
@@ -185,7 +226,9 @@ function startQuiz() {
         answer4.style.fontWeight = "normal";
         answer4.style.backgroundColor = "#303C6C";
         answer4.style.color = "#B4DFE5";
-    }
+        userChoice.push(2)
+        console.log(userChoice)
+    };
     answer4.onclick = () => {
         answer1.style.border = "1px black solid";
         answer1.style.fontWeight = "normal";
@@ -203,37 +246,54 @@ function startQuiz() {
         answer4.style.fontWeight = "normal"
         answer4.style.color = "#303C6C";
         answer4.style.backgroundColor = "#ff85e3";
+        userChoice.push(3)
+        console.log(userChoice)
     };
 }
 
 startButton.onclick = startQuiz;
 
 function change() {
-    if(questionCounter < numOfQuestions) {
+    nextQuestion;
+    console.log(nextQuestion);
+    if (questionCounter < numOfQuestions) {
+        console.log(nextQuestion);
         questionCounter++;
         let questionPage = `
         <main id="flexContainer">
-        <p id="question">${questions[questionCounter].question}</p>
-        <img id="quizPic" src="${questions[questionCounter].image}" alt="Question Image">
+        <p id="question">${
+            questions[questionCounter].question
+        }</p>
+        <img id="quizPic" src="${
+            questions[questionCounter].image
+        }" alt="Question Image">
         <div id="flexAnswers">
         <div>
-        <p class="answer" id="answer1">${questions[questionCounter].answers[0]}</p>
+        <p class="answer" id="answer1">${
+            questions[questionCounter].answers[0]
+        }</p>
         </div>
         <div>
-        <p class="answer" id="answer2">${questions[questionCounter].answers[1]}</p>
+        <p class="answer" id="answer2">${
+            questions[questionCounter].answers[1]
+        }</p>
         </div>
         <div>
-        <p class="answer" id="answer3">${questions[questionCounter].answers[2]}</p>
+        <p class="answer" id="answer3">${
+            questions[questionCounter].answers[2]
+        }</p>
         </div>
         <div>
-        <p class="answer"id="answer4">${questions[questionCounter].answers[3]}</p>
+        <p class="answer"id="answer4">${
+            questions[questionCounter].answers[3]
+        }</p>
         </div>
         </div>    
         <button id="nextQuestion" onclick="change()">Next Question</button>
         </main>            
         `;
         flexContainer.innerHTML = questionPage;
-        
+
         answer1.onclick = () => {
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
@@ -249,9 +309,11 @@ function change() {
             answer3.style.color = "#B4DFE5"
             answer4.style.border = "1px black solid";
             answer4.style.fontWeight = "normal";
-            answer4.style.backgroundColor = "#303C6C"
-            answer4.style.color = "#B4DFE5"
-        }
+            answer4.style.backgroundColor = "#303C6C";
+            answer4.style.color = "#B4DFE5";
+            userChoice.push(0);
+            console.log(userChoice);
+        };
         answer2.onclick = () => {
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
@@ -269,7 +331,9 @@ function change() {
             answer4.style.fontWeight = "normal";
             answer4.style.backgroundColor = "#303C6C";
             answer4.style.color = "#B4DFE5";
-        }
+            userChoice.push(1);
+            console.log(userChoice);
+        };
         answer3.onclick = () => {
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
@@ -287,7 +351,9 @@ function change() {
             answer4.style.fontWeight = "normal";
             answer4.style.backgroundColor = "#303C6C";
             answer4.style.color = "#B4DFE5";
-        }
+            userChoice.push(2);
+            console.log(userChoice);
+        };
         answer4.onclick = () => {
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
@@ -305,30 +371,37 @@ function change() {
             answer4.style.fontWeight = "normal"
             answer4.style.color = "#303C6C";
             answer4.style.backgroundColor = "#ff85e3";
-        };    
+            userChoice.push(3);
+            console.log(userChoice);
+        };
+        let correct = 0;
+        if (userChoice.pop() === questions[questionCounter].rightAnswer) {
+            correct++;
+            console.log(correct);
+        }
     }
 }
 
 // function userChoice() {
-    //     while(questionCounter < numOfQuestions) {
-        //         if(questions[questionCounter].answers[0].onclick) {
-            //             submittedAnswers.push(0);
-            //         } else if(questions[questionCounter].answers[1].onclick) {
-                    //             submittedAnswers.push(1);
-                    //         } else if(questions[questionCounter].answers[2].onclick) {
-                        //             submittedAnswers.push(2);
-                        //         } else if(questions[questionCounter].answers[0].onclick) {
-        //             submittedAnswers.push(3);
-        //         }
-        //     }
-        // };
+//     while(questionCounter < numOfQuestions) {
+//         if(questions[questionCounter].answers[0].onclick) {
+//             submittedAnswers.push(0);
+//         } else if(questions[questionCounter].answers[1].onclick) {
+//             submittedAnswers.push(1);
+//         } else if(questions[questionCounter].answers[2].onclick) {
+//             submittedAnswers.push(2);
+//         } else if(questions[questionCounter].answers[0].onclick) {
+//             submittedAnswers.push(3);
+//         }
+//     }
+// };
 
-    // function checkAnswer() {
-    //     while (questionCounter < numOfQuestions) {
+// function checkAnswer() {
+//     while (questionCounter < numOfQuestions) {
 
-    //     }
-    // };
-            
+//     }
+// };
+
 
 /*************************/
 /********ON-CLICK*********/
