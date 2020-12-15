35a58bab11997dd5a27d05dd6f865b34bad94aa8
Ryan Slayen <ryan_slayen@yahoo.com>
2020-12-15 17:55:19 +0100

    final commit

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ let question = document.getElementById("question");
 let quizPic = document.getElementById("quizPic");
 let nextQuestion;
 let questionContainer = document.getElementById("questionContainer");
+let flexContainer = document.getElementById("flexContainer");
 let nextButton = document.getElementById("nextQuestion");
 let startButton = document.getElementById("startButton");
+let retakeQuizBtn = document.getElementById("retakeQuizBtn");
 let submittedAnswers = [];
 let userChoice;
 let score = 0;
@@ -32,7 +34,7 @@ let images = [
     "./img/microwave.jpeg",
     "./img/virus.png",
     "./img/cow.jpg",
-    "./img/html.jpg"
+    "./img/beer.jpeg"
 ];
 
 
@@ -83,16 +85,16 @@ let questions = [
         question: "How old is the oldest living tree?",
         image: "./img/oldTree.jpg",
         answers: [
-            "4,851 year old Methusela tree", "4,500 year old Mountain Ash", "8,359 year old Yellow Meranti", "5,950 year old Redwood"
+            "4,851 year old Bristlecon Pine", "4,500 year old Mountain Ash", "8,359 year old Yellow Meranti", "5,950 year old Redwood"
         ],
         rightAnswer: 0
     }, {
         question: "What year was the first microwave sold?",
         image: "./img/microwave.jpeg",
         answers: [
-            "December 1996", "July, 1992", "January, 1994", "October, 1995"
+            "1987", "1992", "1967", "1974"
         ],
-        rightAnswer: 3
+        rightAnswer: 2
     }, {
         question: "What is Covid-19?",
         image: "./img/virus.png",
@@ -108,12 +110,12 @@ let questions = [
         ],
         "rightAnswer": 1
     }, {
-        question: "Is HTML considered a programming language?",
-        image: "./img/html.jpg",
+        question: "Which Country drinks the most beer per capita?",
+        image: "./img/beer.jpeg",
         answers: [
-            "Yes", "No", "Maybe", "I don't know..."
+            "Germany", "Czech Republic", "Ireland", "USA"
         ],
-        rightAnswer: 0
+        rightAnswer: 1
     }
 ];
 let numOfQuestions = questions.length;
@@ -134,74 +136,24 @@ nextButton.addEventListener("click", goToNextQuestion);
 
 function runQuiz() {
     startButton.style.display = "none";
+    retakeQuizBtn.style.display = "none";
     nextButton.style.display = "flex";
-    console.log(questionCounter);
+    console.log("runQuiz")
     change(); 
 }
 
-function setUserChoice(value) {
-    userChoice = value;
-}
-
-function getUserChoice() {
-    return userChoice;
-}
-
-function goToNextQuestion() {
-    if (questionCounter < numOfQuestions) {
-        console.log(questionPage);
-        change();
-    } else {
-        alert(`You finished with ${score} correct!`);
-    }
-} 
-
-function questionPage(counter) {
-    return `
-    <div>
-    <p id="question">${
-        questions[counter].question
-    }</p>
-    <img id="quizPic" src="${
-        questions[counter].image
-    }" alt="Question Image">
-    <div id="flexAnswers">
-    <div>
-    <p class="answer" id="answer1" value="0">${
-        questions[counter].answers[0]
-    }</p>
-    </div>
-    <div>
-    <p class="answer" id="answer2" value="1">${
-        questions[counter].answers[1]
-    }</p>
-    </div>
-    <div>
-    <p class="answer" id="answer3" value="2">${
-        questions[counter].answers[2]
-    }</p>
-    </div>
-    <div>
-    <p class="answer"id="answer4" value="3">${
-        questions[counter].answers[3]
-    }</p>
-    </div>
-    </div>          
-    `;
-}
-
 function change() {
+    console.log("function change");
+    console.log(`question counter: ${questionCounter}`);    
     if (questionCounter < numOfQuestions) {
-        console.log(questionCounter);
         questionContainer.innerHTML = questionPage(questionCounter);
         let answer1 = document.getElementById("answer1");
         let answer2 = document.getElementById("answer2");
         let answer3 = document.getElementById("answer3");
         let answer4 = document.getElementById("answer4");
-        console.log("change answer1");
-        console.log(answer1);
+        // console.log("change answer1");
+        // console.log(answer1);
         answer1.onclick = () => {
-            console.log("Iǘe been clicked");
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
             answer1.style.backgroundColor = "#b185ff"
@@ -218,13 +170,14 @@ function change() {
             answer4.style.fontWeight = "normal";
             answer4.style.backgroundColor = "#303C6C";
             answer4.style.color = "#B4DFE5";
-            console.log(event.target);
-            console.log(event.target.value);
-            setUserChoice(0);
-            console.log(getUserChoice());
+            userChoice = 0;
+            // console.log(event.target);
+            console.log("correct answer: "+questions[questionCounter].rightAnswer);
+            console.log("userChoice: "+userChoice);
+            console.log("questionCounter: "+questionCounter);
         };
-        console.log("change answer2");
-        console.log(answer2);
+        // console.log("change answer2");
+        // console.log(answer2);
         answer2.onclick = () => {
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
@@ -243,10 +196,13 @@ function change() {
             answer4.style.backgroundColor = "#303C6C";
             answer4.style.color = "#B4DFE5";
             userChoice = 1;
-            console.log(userChoice);
+            // console.log(event.target);
+            console.log("correct answer :"+questions[questionCounter].rightAnswer);
+            console.log("userChoice:" +userChoice);
+            console.log("questionCounter: "+questionCounter);
         };
-        console.log("change answer3");
-        console.log(answer3);
+        // console.log("change answer3");
+        // console.log(answer3);
         answer3.onclick = () => {
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
@@ -265,10 +221,12 @@ function change() {
             answer4.style.backgroundColor = "#303C6C";
             answer4.style.color = "#B4DFE5";
             userChoice = 2;
-            console.log(userChoice);
+            console.log("correct answer: "+questions[questionCounter].rightAnswer);
+            console.log("user choice:" +userChoice);
+            console.log("questionCounter: "+questionCounter);
         };
-        console.log("change answer4");
-        console.log(answer4);
+        // console.log("change answer4");
+        // console.log(answer4);
         answer4.onclick = () => {
             answer1.style.border = "1px black solid";
             answer1.style.fontWeight = "normal";
@@ -287,47 +245,110 @@ function change() {
             answer4.style.color = "#303C6C";
             answer4.style.backgroundColor = "#ff85e3";
             userChoice = 3;
-            console.log(userChoice);
+            // console.log(event.target);
+            console.log("questionCounter: "+questionCounter);
+            console.log("correct answer: "+questions[questionCounter].rightAnswer);
+            console.log("userChoice: "+userChoice);
         }
-        console.log(userChoice);
-        // let finalChoice = userChoice[userChoice.length - 1];
-        let finalChoice = getUserChoice();
-        let correctAnswer = questions[questionCounter].rightAnswer;
-        console.log("Did we get the answer correct?")
-        console.log(finalChoice);
-        console.log(correctAnswer);
-        console.log(finalChoice === correctAnswer);
-        if (finalChoice === correctAnswer) {
-            console.log("score");
-            console.log(score);
-            score++;
-            console.log(score);
-        }
-        questionCounter++;
+        console.log("selected: "+userChoice);
     }
+    endQuiz();
 };
 
-function endQuiz() {
-
-    let resultsPage = `
-    <main id="flexContainer">
-    <p id="question">You made it to the end of the Quiz!</p>
-    <img id="quizPic" src="./img/allDone.gif" alt="Done and Done">
-    <div id="resultSection">
-    <p class="answer" id="results">Number correct: ${correct}</p>
-    <button id="nextQuestion" onclick="runQuiz()">Retake Quiz</button>
-    </main>            
-    </div>    
-    `;
-    questionContainer.innerHTML = resultsPage;
+function checkAnswer() {
+    let correctAnswer = questions[questionCounter].rightAnswer;
+    console.log("***************************");
+    console.log(userChoice);
+    console.log("Correct answer: "+correctAnswer);
+    console.log(`Correct?: ${userChoice === correctAnswer}`);
+    if (userChoice === correctAnswer) {
+        score++;
+        console.log("well done, current score: "+score);
+    }
 }
 
+function setUserChoice(value) {
+    userChoice = value;
+    console.log("userChoice set");
+}
 
-// function checkAnswer() {
-//     while (questionCounter < numOfQuestions) {
+function getUserChoice() {
+    return userChoice;
+}
 
-//     }
-// };
+function goToNextQuestion() {
+    console.log('score: '+score)
+    checkAnswer();
+    if (questionCounter < numOfQuestions) {
+        // console.log(questionPage);
+        questionCounter++;
+        change();
+    } else {
+        alert(`You finished with ${score} correct!`);
+    }
+    console.log("questionCounter: "+questionCounter);
+} 
+
+function questionPage(questionCounter) {
+    return `
+    <div id="questionContainer">
+    <p id="question">${
+        questions[questionCounter].question
+    }</p>
+    <img id="quizPic" src="${
+        questions[questionCounter].image
+    }" alt="Question Image">
+    <div id="flexAnswers">
+    <div>
+    <p class="answer" id="answer1" value="0">${
+        questions[questionCounter].answers[0]
+    }</p>
+    </div>
+    <div>
+    <p class="answer" id="answer2" value="1">${
+        questions[questionCounter].answers[1]
+    }</p>
+    </div>
+    <div>
+    <p class="answer" id="answer3" value="2">${
+        questions[questionCounter].answers[2]
+    }</p>
+    </div>
+    <div>
+    <p class="answer"id="answer4" value="3">${
+        questions[questionCounter].answers[3]
+    }</p>
+    </div>
+    </div>
+    </div>          
+    `;
+}
+
+function endQuiz() {
+    if(questionCounter === 10) {
+        retakeQuizBtn.style.display = "flex";
+        startButton.style.display = "none";
+        nextButton.style.display = "none";
+        console.log("function endQuiz")
+        let resultsPage = `
+        <main id="questionContainer">
+            <p id="question">You made it to the end of the Quiz!</p>
+            <img id="quizPic" src="./img/allDone.gif" alt="Done and Done">
+            <div id="resultSection">
+                <p class="answer" id="results">Number correct: ${score}</p>
+                <button href="" id="retakeQuizBtn" onclick='retakeQuiz()'>Retake Quiz</button>
+            </div>    
+        </main>            
+        `;
+        questionContainer.innerHTML = resultsPage;
+    }
+}
+
+function retakeQuiz() {
+    if(retakeQuizBtn) {
+        location.reload()
+    }
+}
 
 
 /*************************/
